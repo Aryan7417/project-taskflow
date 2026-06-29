c35e5781867d0cbbc90186f6db362faafa0edd10
Aryan Yadav <caryan830@gmail.com>
2026-06-29 13:26:06 +0530

    axios update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: window.location.origin.includes('localhost:5173')
+  baseURL: window.location.origin.includes('https://project-taskflow.onrender.com')
     ? '/api'
-    : 'http://localhost:5000/api',
+    : 'https://project-taskflow.onrender.com',
   withCredentials: true,
 });
 
